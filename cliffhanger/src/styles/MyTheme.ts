f502e2a114535dade8379c8cf65374909012bda8
vivienelisabethberg <vivien-berg@gmx.de>
2023-11-17 15:26:03 +0100

    initial styles

---
+++ cliffhanger/src/styles/MyTheme.ts
@@ -0,0 +1,14 @@
+import {createTheme} from '@mui/material/styles'
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        light: '#AE26F2',
+        main: '#320448',
+        dark: '#1D0887'
+      },
+      error:{
+        main: '#d32f2f'
+      }
+    }
+})
