9f2da0f157a7f04be3fa95b57835a07e375d4dd5
Eren Ceviz <oezguen.ceviz@student.hs-rm.de>
2024-01-26 13:33:39 +0100

    redirect onclick to moviepage possible

--- cliffhanger/next.config.js
+++ cliffhanger/next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: '/movie/:id',
+        destination: '/movie/[id]',
+        permanent: true,
+      },
+    ];
+  },
+};
