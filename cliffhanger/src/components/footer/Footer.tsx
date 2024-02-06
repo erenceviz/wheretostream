5299b942a2d74775fc427135c544fc775e8cc135
Vivien Berg <vivien.berg@student.hs-rm.de>
2024-02-06 23:48:14 +0100

    get movieQuotes from mock data now

--- cliffhanger/src/components/footer/Footer.tsx
+++ cliffhanger/src/components/footer/Footer.tsx
@@ -2,31 +2,34 @@ import { useState, useEffect } from "react";
 import Logo from "@/components/header/Logo/Logo";
 import styles from "./Footer.module.css";
 import MovieQuote from "@/types/MovieQuote";
+import { movieQuotesData } from "@/types/MovieQuote";
 
 const Footer = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [movieQuotes, setMovieQuote] = useState<MovieQuote[]>([]);
+  const [randomQuotes, setRandomQuotes] = useState<MovieQuote[]>([]);
 
   useEffect(() => {
-    const getMovieQuotes = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(
-          `https://trial-api-popular-movie-quote-2t9j.gw.openapihub.com/movie-quote/quote-by-type?type=movie&X-OpenAPIHub-Key=e618778939d24b50a4eab064346cd55a`
-        );
-        const movieQuotes: MovieQuote[] = await response.json();
-        const firstThreeObjects = movieQuotes.slice(0, 5);
-        console.log(firstThreeObjects);
-        // console.log("Movie Quotes: ", movieQuotes);
-        setMovieQuote(firstThreeObjects);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setIsLoading(false);
+    const getRandomQuotesOutOfTen = (quotes: MovieQuote[]) => {
+      if (quotes.length < 5) {
+        console.error("Die Liste der Zitate ist zu kurz.");
+        return [];
       }
+
+      const selectedQuotes: MovieQuote[] = [];
+
+      while (selectedQuotes.length < 5) {
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+
+        // Vermeidung von Dupikaten (index)
+        if (!selectedQuotes.some(quote => quote === quotes[randomIndex])) {
+          selectedQuotes.push(quotes[randomIndex]);
+        }
+      }
+
+      return selectedQuotes;
     };
 
-    getMovieQuotes();
+    const randomQuotesResult = getRandomQuotesOutOfTen(movieQuotesData);
+    setRandomQuotes(randomQuotesResult);
   }, []);
 
   return (
@@ -36,12 +39,12 @@ const Footer = () => {
         <div className={styles.seperationline}></div>
         <div className={styles.quotes}>
           <div className={styles.quote}>
-            {movieQuotes?.map((quote) => (
-              <div className={styles.singleQuote} key={quote.quote}>
+            {randomQuotes.map((quote, index) => (
+              <div className={styles.singleQuote} key={index}>
+                <p>{quote.quote}</p>
                 <p>
-                  {quote.quote}
+                  {quote.movie}, {quote.year}
                 </p>
-                <p>{quote.movie}, {quote.year}</p>
               </div>
             ))}
           </div>
--- cliffhanger/src/types/MovieQuote.tsx
+++ cliffhanger/src/types/MovieQuote.tsx
@@ -1,6 +1,69 @@
 export default interface MovieQuote {
-    quote: string;
-    movie: string;
-    type: "movie";
-    year: number;
-};
+  quote: string;
+  movie: string;
+  type: "movie";
+  year: number;
+}
+
+export const movieQuotesData: MovieQuote[] = [
+  {
+    quote: "I see dead people.",
+    movie: "The Sixth Sense",
+    type: "movie",
+    year: 1999,
+  },
+  {
+    quote: "Why so serious?",
+    movie: "The Dark Knight",
+    type: "movie",
+    year: 2008,
+  },
+  {
+    quote: "To infinity and beyond!",
+    movie: "Toy Story",
+    type: "movie",
+    year: 1995,
+  },
+  {
+    quote: "I'm the king of the world!",
+    movie: "Titanic",
+    type: "movie",
+    year: 1997,
+  },
+  {
+    quote: "You complete me.",
+    movie: "Jerry Maguire",
+    type: "movie",
+    year: 1996,
+  },
+  {
+    quote: "I'll have what she's having.",
+    movie: "When Harry Met Sally",
+    type: "movie",
+    year: 1989,
+  },
+  {
+    quote: "I am your father.",
+    movie: "Star Wars: Episode V - The Empire Strikes Back",
+    type: "movie",
+    year: 1980,
+  },
+  {
+    quote: "May the Force be with you.",
+    movie: "Star Wars",
+    type: "movie",
+    year: 1977,
+  },
+  {
+    quote: "You can't handle the truth!",
+    movie: "A Few Good Men",
+    type: "movie",
+    year: 1992,
+  },
+  {
+    quote: "Houston, we have a problem.",
+    movie: "Apollo 13",
+    type: "movie",
+    year: 1995,
+  },
+];
