89d133a17b9636ac2d376914c98d9561ccf8a317
ocevi001 <oezguen.ceviz@student.hs-rm.de>
2024-02-09 20:45:37 +0100

    fix detailspage

--- cliffhanger/src/components/moviedetails/DetailsPage.tsx
+++ cliffhanger/src/components/moviedetails/DetailsPage.tsx
@@ -172,13 +172,10 @@ export function DetailsPage({ itemId }: IDetailsPage) {
   //   }};
 
   const movieInList: boolean = Boolean(
-    (bookmarkState.movies &&
+    bookmarkState.movies &&
       itemId &&
-      bookmarkState.movies.find((ele) => ele.id === parseInt(itemId as string))) ||
-        (movieId &&
-          bookmarkState.movies.find((ele) => ele.id === parseInt(movieId as string)))
-      );
-      
+      bookmarkState.movies.find((ele) => ele.id === parseInt(itemId as string))
+  );
 
   const toggleBookmark = () => {
     dispatch({
@@ -267,98 +264,7 @@ export function DetailsPage({ itemId }: IDetailsPage) {
           )}
         </Modal>
       )}
-      <div className={styles.posterAndInfo}>
-        <img
-          className={styles.poster}
-          src={
-            (movieData && movieData?.poster_path) || (tvData && tvData?.poster_path)
-              ? `https://image.tmdb.org/t/p/w300${movieData?.poster_path || tvData?.poster_path}`
-              : ""
-          }
-        />
-        <div className={styles.infoToPoster}>
-          <div className={styles.headlineWithBookmark}>
-            <h1>{movieData?.title || tvData?.name}</h1>
-            <svg
-              style={{ height: "40px", width: "auto", marginLeft: "auto" }}
-              onClick={toggleBookmark}
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0,0,256,256"
-              width="60px"
-              height="60px"
-            >
-              <g
-                fill={movieInList ? "gold" : "white"}
-                fillRule="nonzero"
-                stroke="none"
-                strokeWidth="1"
-                strokeLinecap="butt"
-                strokeLinejoin="miter"
-                strokeMiterlimit="10"
-                strokeDasharray=""
-                strokeDashoffset="0"
-                fontFamily="none"
-                fontWeight="none"
-                fontSize="none"
-                textAnchor="none"
-                style={{ mixBlendMode: "normal" }}
-              >
-                <g transform="scale(8.53333,8.53333)">
-                  <path d="M23,27l-8,-7l-8,7v-22c0,-1.105 0.895,-2 2,-2h12c1.105,0 2,0.895 2,2z"></path>
-                </g>
-              </g>
-            </svg>
-          
-          <h4 className={styles.h4}>{movieData?.runtime || (tvData && tvData?.episode_run_time[0])} minutes</h4>
-          <p style={{ marginTop: "20px" }}>{movieData?.overview || tvData?.overview}</p>
-   
-              <p className={styles.movieMetaData}>
-                {movieData?.release_date.slice(0, 4)}
-              </p>
-            </div>
-            <Button
-              className={styles.playTrailerBtn}
-              variant="contained"
-              onClick={() => setIsModalOpen(true)}
-            >Play Trailer
-            </Button>
-          </div>
-        </div>
-      </div>
-      <div className={styles.movieDetails}>
-        {isModalOpen && (
-          <Modal
-            isOpen={isModalOpen}
-            onRequestClose={() => setIsModalOpen(false)}
-            contentLabel="Trailer Modal"
-            style={{
-              overlay: {
-                position: 'fixed',
-                backgroundColor: 'rgba(0, 0, 0, 0.75)',
-                zIndex: 1000
-              },
-              content: {
-                position: 'absolute',
-                top: '50 %',
-                left: '50 %',
-                border: 'none',
-                background: 'transparent',
-                overflow: 'auto',
-                outline: 'none',
-              }
-            }}
-          >
-            {trailerKey && (
-              <iframe
-                width="750"
-                height="420"
-                src={`https://www.youtube.com/embed/${trailerKey}`}
-                title="YouTube video player"
-                allowFullScreen
-              ></iframe>
-            )}
-          </Modal>
-        )}
+      
         <div className={styles.posterAndInfo}>
           <img
             className={styles.poster}
